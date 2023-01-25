8369d44e4752b214c7393600534ccca3926eec21
JFranCL <61494023+JFranCL@users.noreply.github.com>
2023-01-25 11:32:36 +0100

    Update NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,7 +1,7 @@
 package example;		
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
+//import org.testng.AssertJUnit;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.firefox.FirefoxDriver;		
